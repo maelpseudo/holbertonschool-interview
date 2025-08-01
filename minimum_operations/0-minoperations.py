0473cc653eba22e65140ba8bec8666ab933ad866
Maël PRO <maelpro@MacBook-Air-de-Mael.local>
2025-08-01 12:36:53 +0200

    Added min operations project

---
+++ minimum_operations/0-minoperations.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+import shutil, os
+
+def minOperations(n):
+    if n <= 1:
+        return 0
+
+    operations = 0
+    # extraire les 2
+    while n % 2 == 0:
+        operations += 2  # facteur 2 coûte 2 opérations
+        n //= 2
+
+    # extraire les facteurs impairs
+    p = 3
+    while p * p <= n:
+        while n % p == 0:
+            operations += p  # facteur p coûte p opérations
+            n //= p
+        p += 2
+
+    # s'il reste un facteur premier > 1
+    if n > 1:
+        operations += n
+
+    return operations
